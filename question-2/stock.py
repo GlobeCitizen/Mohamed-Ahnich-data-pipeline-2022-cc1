ddd952c0166c015c4e56ac82867d5ed099b59e68
mohamed.ahnich <ahnich97@gmail.com>
2022-12-06 16:55:07 +0100

    write data to kinesis stream

--- question-2/stock.py
+++ question-2/stock.py
@@ -3,7 +3,7 @@ import json
 import random
 import boto3
 
-STREAM_NAME = "input-stream"
+STREAM_NAME = "Mohamed-Ahnich-stock-input-stream"
 REGION = "us-east-1"
 
 def get_data():
@@ -15,8 +15,7 @@ def get_data():
 def generate(stream_name, kinesis_client):
     while True:
         data = get_data()
-        print(data)
-       #kinesis_client.put_record(StreamName=stream_name,Data=json.dumps(data),PartitionKey="partitionkey")
+        kinesis_client.put_record(StreamName=stream_name,Data=json.dumps(data),PartitionKey="partitionkey")
 
 if __name__ == '__main__':
     generate(STREAM_NAME, boto3.client('kinesis', region_name=REGION))
